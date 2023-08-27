99e73f682052fb795b5b721a0fdccb48b2da6d5c
Gil Shapira <gshapira@gmail.com>
2023-08-27 20:21:36 +0300

    Add support for UpdateOptions when updating email/phone

--- lib/src/types/others.dart
+++ lib/src/types/others.dart
@@ -1,4 +1,3 @@
-
 /// The delivery method for an OTP or Magic Link message.
 enum DeliveryMethod {
   email,
@@ -68,4 +67,43 @@ class SignInOptions {
   final Map<String, dynamic> customClaims;
 
   const SignInOptions({this.stepupRefreshJwt, this.mfaRefreshJwt, this.customClaims = const {}});
+}
+
+/// Used to configure how users are updated.
+class UpdateOptions {
+  /// Whether to allow sign in from a new `loginId` after an update.
+  ///
+  /// When a user's email address or phone number are updated and this is set to `true`
+  /// the new value is added to the user's list of `loginIds`, and the user from that
+  /// point on will be able to use it to sign in.
+  final bool addToLoginIds;
+
+  /// Whether to keep or delete the current user when merging two users after an update.
+  ///
+  /// When updating a user's email address or phone number and with `addToUserLoginIds`
+  /// set to `true`, if another user in the the system already has the same email address
+  /// or phone number as the one being added in their list of `loginIds` the two users
+  /// are merged and one of them is deleted.
+  ///
+  /// This scenario can happen when a user uses multiple authentication methods
+  /// and ends up with multiple accounts. For example, a user might sign in with
+  /// their email address at first. Then at some point later they reinstall the
+  /// app and use OAuth to authenticate, and a new user account is created. If
+  /// the user then updates their account and adds their email address the
+  /// two accounts need to be merged.
+  ///
+  /// Let's define the "updated user" to be the user being updated and whom
+  /// the `refreshJwt` belongs to, and the "existing user" to be another user in
+  /// the system with the same `loginId`.
+  ///
+  /// By default, the updated user is kept, the existing user's details are merged
+  /// into the updated user, and the existing user is then deleted.
+  ///
+  /// If this option is set to `true` however then the updated user is merged into
+  /// the existing user, and the updated user is deleted. In this case the [DescopeSession]
+  /// and its `refreshJwt` that was used to initiate the update operation will no longer
+  /// be valid, and an [AuthenticationResponse] is returned for the existing user instead.
+  final bool onMergeUseExisting;
+
+  const UpdateOptions({this.addToLoginIds = false, this.onMergeUseExisting = false});
 }
