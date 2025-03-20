4bebcdb9d00d3a77aef3904d979160b501560778
itaihanski <itaihanski@gmail.com>
2025-03-20 11:38:17 +0200

    Bump version to 0.9.6

--- lib/src/sdk/sdk.dart
+++ lib/src/sdk/sdk.dart
@@ -22,7 +22,7 @@ class DescopeSdk {
   static const name = 'DescopeFlutter';
 
   /// The Descope SDK version
-  static const version = '0.9.5';
+  static const version = '0.9.6';
 
   /// The configuration of the [DescopeSdk] instance.
   final DescopeConfig config;
