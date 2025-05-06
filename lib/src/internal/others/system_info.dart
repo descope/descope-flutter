ffda0e71855d8c6277cb1bb4280581b5abae3982
Gil Shapira <gshapira@gmail.com>
2025-05-06 15:56:04 +0300

    Cleanup

--- lib/src/internal/others/system_info.dart
+++ lib/src/internal/others/system_info.dart
@@ -25,10 +25,14 @@ class SystemInfo {
   static SystemInfo? _instance;
 
   static Future<SystemInfo> _create() async {
-    if (kIsWeb) {
-      return await _createWeb();
+    try {
+      if (kIsWeb) {
+        return await _createWeb();
+      }
+      return await _createMobile();
+    } catch (e) {
+      return SystemInfo._();
     }
-    return await _createMobile();
   }
 
   // Mobile
@@ -38,16 +42,12 @@ class SystemInfo {
   static Future<SystemInfo> _createMobile() async {
     final systemInfo = SystemInfo._();
     if (Platform.isIOS || Platform.isAndroid) {
-      try {
-        Map<Object?, Object?> data = await _mChannel.invokeMethod('getSystemInfo');
-        systemInfo.platformName = data['platformName'] as String? ?? '';
-        systemInfo.platformVersion = data['platformVersion'] as String? ?? '';
-        systemInfo.appName = data['appName'] as String? ?? '';
-        systemInfo.appVersion = data['appVersion'] as String? ?? '';
-        systemInfo.device = data['device'] as String? ?? '';
-      } catch (e) {
-        // ignore
-      }
+      Map<Object?, Object?> data = await _mChannel.invokeMethod('getSystemInfo');
+      systemInfo.platformName = data['platformName'] as String? ?? '';
+      systemInfo.platformVersion = data['platformVersion'] as String? ?? '';
+      systemInfo.appName = data['appName'] as String? ?? '';
+      systemInfo.appVersion = data['appVersion'] as String? ?? '';
+      systemInfo.device = data['device'] as String? ?? '';
     }
     return systemInfo;
   }
@@ -59,40 +59,35 @@ class SystemInfo {
   static Future<SystemInfo> _createWeb() async {
     final systemInfo = SystemInfo._();
 
-    try {
-      final agent = window.navigator.userAgent;
-      final vendor = window.navigator.vendor;
+    final agent = window.navigator.userAgent;
+    final vendor = window.navigator.vendor;
 
-      // see: https://github.com/flutter/flutter/blob/7cfaf04fa9781d5287af8b348ea1cc1cf5ef701d/engine/src/flutter/lib/web_ui/lib/ui_web/src/ui_web/browser_detection.dart
-      if (vendor == 'Google Inc.') {
-        systemInfo.platformName = 'chromium';
-      } else if (vendor == 'Apple Computer, Inc.') {
-        systemInfo.platformName = 'webkit';
-      } else if (agent.contains('Edg/')) {
-        systemInfo.platformName = 'edge';
-      } else if (vendor == '' && agent.contains('firefox')) {
-        systemInfo.platformName = 'firefox';
-      }
-
-      final regexp = RegExp(_versionPattern);
-      final match = regexp.firstMatch(agent);
-      if (match != null && match.groupCount == 2) {
-        systemInfo.platformVersion = match[2] ?? '';
-        if (systemInfo.platformName.isEmpty) {
-          final browser = (match[1] ?? '').toLowerCase();
-          if (browser == 'chrome') {
-            systemInfo.platformName = agent.contains('Edg/') ? 'edge' : 'chromium';
-          } else if (browser == 'safari') {
-            systemInfo.platformName = 'webkit';
-          } else {
-            systemInfo.platformName = browser;
-          }
-        }
-      }
-    } catch (e) {
-      // ignore
+    // see: https://github.com/flutter/flutter/blob/7cfaf04fa9781d5287af8b348ea1cc1cf5ef701d/engine/src/flutter/lib/web_ui/lib/ui_web/src/ui_web/browser_detection.dart
+    if (vendor == 'Google Inc.') {
+      systemInfo.platformName = 'chromium';
+    } else if (vendor == 'Apple Computer, Inc.') {
+      systemInfo.platformName = 'webkit';
+    } else if (agent.contains('Edg/')) {
+      systemInfo.platformName = 'edge';
+    } else if (vendor == '' && agent.contains('firefox')) {
+      systemInfo.platformName = 'firefox';
     }
 
+    final regexp = RegExp(_versionPattern);
+    final match = regexp.firstMatch(agent);
+    if (match != null && match.groupCount == 2) {
+      systemInfo.platformVersion = match[2] ?? '';
+      if (systemInfo.platformName.isEmpty) {
+        final browser = (match[1] ?? '').toLowerCase();
+        if (browser == 'chrome') {
+          systemInfo.platformName = agent.contains('Edg/') ? 'edge' : 'chromium';
+        } else if (browser == 'safari') {
+          systemInfo.platformName = 'webkit';
+        } else {
+          systemInfo.platformName = browser;
+        }
+      }
+    }
     return systemInfo;
   }
 }
