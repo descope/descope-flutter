8497a4238bb436543fc30dab7defd82a0febbc2a
Itai Hanski <itaihanski@gmail.com>
2026-01-15 12:23:17 +0200

    Update iOS SDK to 0.10.5 and Android SDK to 0.18.0

--- ios/Classes/descope-swift-sdk/sdk/SDK.swift
+++ ios/Classes/descope-swift-sdk/sdk/SDK.swift
@@ -143,7 +143,7 @@ public extension DescopeSDK {
     static let name = "DescopeKit"
     
     /// The Descope SDK version
-    static let version = "0.10.4"
+    static let version = "0.10.5"
 }
 
 // Internal
